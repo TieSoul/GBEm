e6c4209364427008e9495b0bde4b5f8e2f44271c
TieSoul <tdbaksteen@gmail.com>
2016-12-21 11:14:44 +0100

    Removed testing code from main kek

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,5 @@ use cpu::CPU;
 
 
 fn main() {
-    let mut cpu = CPU::new();
-    let r1 = cpu.alu_add(128, 127);
-    let r2 = cpu.alu_adc(128,128);
-    let r3 = cpu.alu_adc(128,0);
-    println!("{}", r1);
-    println!("{}", r2);
-    println!("{}", r3);
+
 }
